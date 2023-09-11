d3b2ea450504eadcec6cc915eec8936d6165cd45
eunheeko <eunhee.ko.astro@gmail.com>
2023-09-11 14:21:27 +0900

    wrote duolingo modules

--- atcorner/duolingo.py
+++ atcorner/duolingo.py
@@ -1,4 +1,34 @@
-# https://github.com/yihong0618/2023/blob/main/github_daily/duolingo.py
-# This is for keeping up my duolingo status
+import requests
 
+def _get_duolingo(user_name, password):
+
+    s = requests.Session():
+    r = s.post(
+        "https://www.duolingo.com/login",
+        params = {"login": user_name, 
+                  "password": password}
+    )
+
+    if r.status_code != 200:
+        raise Exception("Login failed")
+    
+    name = r.json()["username"]
+
+    return s, name
+
+def duolingo(user_name, password):
+
+    s, name = _get_duolingo(user_name, password)
+    r = s.get(f"https://www.duolingo.com/users/{name}")
+
+    if r.status_code != 200:
+        raise Exception("Get profile failed")
+    
+    data = r.json()
+
+    streak = data["site_streak"]
+    language = data["learning_language"]
+    total = data["language_data"].get(language, {}).get("level_progress", 0)
+
+    return total, streak
 
